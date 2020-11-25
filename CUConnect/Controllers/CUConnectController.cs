dad5783de9b4c2144784faac5993ff1c95f861a9
Corey Fuller <coreyfuller00@gmail.com>
2020-11-25 15:22:23 -0500

    Add project files.

---
+++ CUConnect/Controllers/CUConnectController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CUConnect.Models;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace CUConnect.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CUConnectController : ControllerBase
+    {
+
+        private readonly ILogger<CUConnectController> _logger;
+
+        public CUConnectController(ILogger<CUConnectController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<Student> Get()
+        {
+            StreamReader r = new StreamReader(@"C:\Users\corey\Desktop\CUConnect\CUConnect\Data\db.json");
+            string json = r.ReadToEnd();
+            List<Student> students = JsonConvert.DeserializeObject<List<Student>>(json);
+            return students;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Student student)
+        {
+            int x = 1;
+            Console.WriteLine(student.ToString());
+        }
+    }
+}
---
+++ CUConnect/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CUConnect.Models
+{
+    public partial class Student
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("UID")]
+        public long Uid { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("classes")]
+        public List<string> Classes { get; set; }
+
+        [JsonProperty("hobbies")]
+        public List<string> Hobbies { get; set; }
+
+        [JsonProperty("connections")]
+        public List<object> Connections { get; set; }
+    }
+}
